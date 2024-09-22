f66756b48aca872a461ac394faab96fb36d59f1e
Dominik Cebula <dominikcebula@gmail.com>
2024-09-22 21:29:04 +0200

    Spring Security User Authentication with JPA

---
+++ src/main/java/com/dominikcebula/spring/security/user/authentication/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.dominikcebula.spring.security.user.authentication.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
