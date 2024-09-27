1f6edcb85fbb6f603c4d38992a7d4bf2c11e03ef
Dominik Cebula <dominikcebula@gmail.com>
2024-09-27 21:44:03 +0200

    Spring Security User Authentication with JPA

--- src/main/resources/db/changelog/db.changelog-001-init-db.sql
+++ src/main/resources/db/changelog/db.changelog-001-init-db.sql
@@ -1,6 +1,6 @@
 create table users (
     user_id serial primary key,
-	username varchar(50) not null,
+	username varchar(64) not null,
 	password varchar(64) not null,
 	role varchar(50) not null,
 	enabled boolean not null
