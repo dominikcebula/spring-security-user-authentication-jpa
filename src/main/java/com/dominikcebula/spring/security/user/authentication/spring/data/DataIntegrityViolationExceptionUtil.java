50fad95db5db7b49e4ec3cf0373b32cb4cff2423
Dominik Cebula <dominikcebula@gmail.com>
2024-09-27 22:54:32 +0200

    Spring Security User Authentication with JPA

--- src/main/java/com/dominikcebula/spring/security/user/authentication/spring/data/DataIntegrityViolationExceptionUtil.java
+++ src/main/java/com/dominikcebula/spring/security/user/authentication/spring/data/DataIntegrityViolationExceptionUtil.java
@@ -1,10 +1,11 @@
 package com.dominikcebula.spring.security.user.authentication.spring.data;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 
 public class DataIntegrityViolationExceptionUtil {
     public static boolean isConstraintViolation(String constraintName, DataIntegrityViolationException e) {
-        if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException constraintViolationException)
+        if (e.getCause() instanceof ConstraintViolationException constraintViolationException)
             return constraintName.equals(constraintViolationException.getConstraintName());
 
         return false;
