c8131b88d95374ca87575db5361d0746709b0d8a
Dominik Cebula <dominikcebula@gmail.com>
2024-09-27 21:39:26 +0200

    Spring Security User Authentication with JPA

--- src/main/java/com/dominikcebula/spring/security/user/authentication/spring/security/SecurityConfig.java
+++ src/main/java/com/dominikcebula/spring/security/user/authentication/spring/security/SecurityConfig.java
@@ -1,18 +1,32 @@
 package com.dominikcebula.spring.security.user.authentication.spring.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, PersistentTokenRepository tokenRepository) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .formLogin(form -> form.loginPage("/login"))
                 .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/home"))
+                .rememberMe(rememberMe -> rememberMe.tokenRepository(tokenRepository))
                 .build();
+    }
+
+    @Bean
+    @Autowired
+    public PersistentTokenRepository persistentTokenRepository(DataSource dataSource) {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
     }
 }
--- src/main/resources/db/changelog/db.changelog-001-init-db.sql
+++ src/main/resources/db/changelog/db.changelog-001-init-db.sql
@@ -5,3 +5,10 @@ create table users (
 	role varchar(50) not null,
 	enabled boolean not null
 );
+
+create table persistent_logins (
+    username varchar(64) not null,
+    series varchar(64) primary key,
+    token varchar(64) not null,
+    last_used timestamp not null
+);
